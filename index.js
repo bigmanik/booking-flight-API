ff4e7022a9283797d2ccfdf581ae474952527c1b
bigmanik <igbinomwanhia10@gmail.com>
2022-11-05 01:47:36 -0700

    made few changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs/promises");
 const express = require("express");
 const { json } = require("express");
 const flights = require("./controllers/flightController");
@@ -5,10 +6,18 @@ const models = require("./models/Flight");
 const routes = require("./routes/flightRoute");
 const _ = require("lodash");
 const {v4:uuid} = require("uuid");
+const cors = require("cors");
+
 
 const app = express();
 
-app.use(json());
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use("/", routes);
+
 app.get("/flight", (req, res) => {
   const title = ["flight to canada", "flight to japan", "flight to russia", "flight to USA", "flight to germany" ];
   const  time = ["1pm", "3pm", "6pm", "9pm", "11pm"];
@@ -23,12 +32,35 @@ app.get("/flight", (req, res) => {
 
     });
 });
-app.post("/bookflight", (req, res) =>{
+app.get("/bookflight/,:id", async (req, res) =>{
+    const id = req.params.id;
+    let content;
+    try {
+        content = await fs.readFile(`data/bookflight/${id}.txt`, "utf-8");
+    } catch (err) {
+
+    }
+    res.json({
+        content:content
+    });
+})
+app.post("/bookflight", async (req, res) =>{
     const id = uuid();
-    console.log(id);
-    res.sendStatus(301);
+    const content = req.body.content;
+
+    
+    if(!content){
+        return res.sendStatus(400);
+    }
+
+    await fs.mkdir("data/bookflight", {recursive:true});
+    await fs.writeFile(`data/bookflight/${id}.txt`, content);
+    res.status(201).json({
+        id: id
+    });
+    
 });
-app.use("/", routes);
+
 
 const port = process.env.PORT || 3000;
 
